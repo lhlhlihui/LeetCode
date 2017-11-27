0dc314a436ba6ab7c8edb0675ab8c9c607e76fc9
lhlhlihui <1057460714@qq.com>
2017-11-27 23:10:33 +0800

    去除了多余的语句

--- 35-SearchInsertPosition.js
+++ 35-SearchInsertPosition.js
@@ -1,12 +1,8 @@
 var searchInsert = function(nums, target) {
-    if (target <= nums[0]) {
-        return 0;
-    } else {
-        var i = 0;
-        while (nums[i] < target) {
-            i++;
-        }
-
-        return i;
+    var i = 0;
+    while (nums[i] < target) {
+        i++;
     }
+
+    return i;
 };
