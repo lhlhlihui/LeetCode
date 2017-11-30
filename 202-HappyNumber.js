0ebfcbcf1781171ec5a5e5a1d13f365129547e4c
lhlhlihui <1057460714@qq.com>
2017-11-30 23:13:02 +0800

    update 202-HappyNumber.js 为变量 n_str 添加了遗漏的 var

--- 202-HappyNumber.js
+++ 202-HappyNumber.js
@@ -7,7 +7,7 @@ var isHappy = function(n) {
 };
 
 var quadraticSum = function(n) {
-    n_str = String(n);
+    var n_str = String(n);
     var sum = 0;
     for (var i = 0; i < n_str.length; ++i) {
         sum += Number(n_str[i]) ** 2;
