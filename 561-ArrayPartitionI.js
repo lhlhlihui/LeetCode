af5cbefd9945b5cad7cf6299a6d6f36105210431
lhlhlihui <1057460714@qq.com>
2017-12-07 22:07:42 +0800

    完成 561

---
+++ 561-ArrayPartitionI.js
@@ -0,0 +1,9 @@
+var arrayPairSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    var sum = 0;
+    for (i = 0; i < nums.length; i += 2) {
+        sum += nums[i];
+    }
+
+    return sum;
+};
