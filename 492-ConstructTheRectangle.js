c7fb673db698ecf70b728407d8668d044cad50fe
lhlhlihui <1057460714@qq.com>
2017-12-07 22:23:53 +0800

    优化 492-ConstructTheRectangle.js

--- 492-ConstructTheRectangle.js
+++ 492-ConstructTheRectangle.js
@@ -1,7 +1,7 @@
 var constructRectangle = function(area) {
     var tmp = Math.floor(Math.sqrt(area));
     var result = [];
-    for (var i = tmp; i > 0; i--) {
+    for (var i = tmp; ; i--) {
         if (area % i === 0) {
             result.push(area / i);
             result.push(i);
